1b992fc2991f4be54dd841b2250bf653a11eb542
Osvaldo Rodriguez Armenta <158524993+OsvaldoRA@users.noreply.github.com>
2025-10-02 17:50:44 +0000

    Pila nodo

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -1,11 +1,22 @@
 package miPrincipal;
-
+import pila.Pila;
 public class Principal {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new Principal().getGreeting());
+        //crear una pila de enteros 
+        Pila<Integer> pila = new Pila<>();
+        //apilamos algunos elementos
+        pila.apilar(2);
+        pila.apilar(5);
+        pila.apilar(7);
+        System.out.println("El tope de la pila es: " + pila.cima());
+        pila.retirar();
+        System.out.println("El tope de la pila es: " + pila.cima());
+        pila.retirar();
+        System.out.println("El tope de la pila es: " + pila.cima());
+        pila.retirar();
+        System.out.println("El tope de la pila es: " + pila.cima());
+        pila.apilar(10);
+        pila.apilar(20);
+        System.out.println("El tamaño de la pila es: " + pila.getTamanio());
     }
 }
---
+++ pila/Pila.java
@@ -0,0 +1,64 @@
+package pila;
+public class Pila<T> {
+    //atributos
+    private Nodo<T> cabeza;
+    private int tamanio;
+    //constructores
+    public Pila() {
+        cabeza = null;
+        tamanio = 0;
+    }
+    //getter y setter
+    public Nodo<T> getCabeza() {
+        return cabeza;
+    }
+    public void setCabeza(Nodo<T> cabeza) {
+        this.cabeza = cabeza;
+    }
+    public int getTamanio() {
+        return tamanio;
+    }
+    public void setTamanio(int tamanio) {
+        this.tamanio = tamanio;
+    }
+    //confirma si la pila esta vacia 
+    public boolean esVacia() {
+        /*if(tamanio == 0){
+            return true;
+        }else{
+            return false;
+        }*/
+        return cabeza == null;
+    }
+    //agrega un nuevo nodo a la pila
+    public void apilar(T valor){
+        Nodo<T> nuevo = new Nodo<>();
+        //fija el valor a nuevo nodo
+        nuevo.setValor(valor);
+        if (esVacia()) {
+            //cabeza apunta al nuevo
+            cabeza = nuevo; 
+        }else{
+            //se enlaza el cambio siguiente de nuevo al nodo cabeza
+            nuevo.setSiguiente(cabeza);
+            //la nueva cabeza de la pila pasa a ser nuevo
+            cabeza = nuevo;
+        }
+        tamanio++;
+    }
+    //elimina el ultimo nodo de la pila 
+    public void retirar(){
+        if(!esVacia()){
+            cabeza = cabeza.getSiguiente();
+            tamanio--;
+        }
+    }
+    //regresa el valor del tope de la fila 
+    public T cima(){
+        if(!esVacia()){
+            return cabeza.getValor();
+        }
+        return null;
+    }
+}
+    
